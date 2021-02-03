0588513f1948c4ddef6c346fd50ad005a28177d0
melserngawy <melserng@redhat.com>
2021-02-03 13:11:01 -0500

    amend
    Signed-off-by: melserngawy <melserng@redhat.com>

--- site_gen.sh
+++ site_gen.sh
@@ -3,7 +3,18 @@
 CLUSTER_NAME=$1
 PROFILE=$2
 
-cat << EOF > ./$CLUSTER_NAME.yaml
+if [ $PROFILE == "cu" ]; then
+  cp -rf manifests/sites/sample-site-cu manifests/sites/${CLUSTER_NAME}
+  sed -i "s/sample-site-cu/$CLUSTER_NAME/g" manifests/sites/${CLUSTER_NAME}/*
+elif [ $PROFILE == "du" ]; then
+  cp -rf manifests/sites/sample-site-du manifests/sites/${CLUSTER_NAME}
+  sed -i "s/sample-site-du/$CLUSTER_NAME/g" manifests/sites/${CLUSTER_NAME}/*
+else
+  echo "Profile should be either cu or du. Profile=" $PROFILE
+  exit 0
+fi
+
+cat << EOF > ./spoke_clusters/$CLUSTER_NAME.yaml
 apiVersion: v1
 kind: Namespace
 metadata:
@@ -45,4 +56,6 @@ spec:
   version: 2.1.0
 EOF
 
-echo "  - "$CLUSTER_NAME".yaml" >> ./kustomization.yaml
+echo "  - "$CLUSTER_NAME".yaml" >> ./spoke_clusters/kustomization.yaml
+
+echo "still need to modify sites/"$CLUSTER_NAME"/04_sriov_network_node_policy.yaml with intf names that match your cluster info"
